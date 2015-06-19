116ae1e518ba4f60c790087e5997662b773fe290
Caleb Lee <caleb.lee@vokalinteractive.com>
2015-06-19 15:24:15 -0500

    Adds table view interface

---
+++ Grocery Store Client/View Controllers/GRSInventoryListViewController.h
@@ -0,0 +1,13 @@
+//
+//  GRSInventoryListViewController.h
+//  Grocery Store Client
+//
+//  Created by Caleb Lee on 2015/06/19.
+//  Copyright (c) 2015年 Vokal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRSInventoryListViewController : UIViewController
+
+@end
---
+++ Grocery Store Client/View Controllers/GRSInventoryListViewController.m
@@ -0,0 +1,49 @@
+//
+//  GRSInventoryListViewController.m
+//  Grocery Store Client
+//
+//  Created by Caleb Lee on 2015/06/19.
+//  Copyright (c) 2015年 Vokal. All rights reserved.
+//
+
+#import "GRSInventoryListViewController.h"
+
+@interface GRSInventoryListViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation GRSInventoryListViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma Mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    
+    
+    return cell;
+}
+
+
+@end
