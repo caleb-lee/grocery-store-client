e9acf82488295db10fda3f1b5bb1e13a77af6de4
Caleb Lee <caleb.lee@vokalinteractive.com>
2015-06-22 11:38:27 -0500

    Removes AFNetworking import

---
+++ Grocery Store Client/Utilities/GRSNetworkAPIUtility.h
@@ -0,0 +1,13 @@
+//
+//  GRSNetworkAPIUtility.h
+//  Grocery Store Client
+//
+//  Created by Caleb Lee on 2015/06/22.
+//  Copyright (c) 2015年 Vokal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GRSNetworkAPIUtility : NSObject
+
+@end
---
+++ Grocery Store Client/Utilities/GRSNetworkAPIUtility.m
@@ -0,0 +1,13 @@
+//
+//  GRSNetworkAPIUtility.m
+//  Grocery Store Client
+//
+//  Created by Caleb Lee on 2015/06/22.
+//  Copyright (c) 2015年 Vokal. All rights reserved.
+//
+
+#import "GRSNetworkAPIUtility.h"
+
+@implementation GRSNetworkAPIUtility
+
+@end
