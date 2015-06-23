75f88154ded3e0988828bec5517019c300965479
Caleb Lee <caleb.lee@vokalinteractive.com>
2015-06-23 12:39:42 -0500

    Adds Product category which allows the product class to communicate with the server; this makes for easy purchasing and restocking (while keeping records synced between the server and client too)

--- Grocery Store Client/Categories/Product+API_Interaction.h
+++ Grocery Store Client/Categories/Product+API_Interaction.h
@@ -7,7 +7,14 @@
 //
 
 #import "Product.h"
+#import "GRSNetworkAPIUtility.h"
 
 @interface Product (API_Interaction)
+
+- (void)purchase:(GRSNetworkNoResponseNetworkCompletionBlock)completion;
+- (void)purchaseQuantitiy:(NSInteger)quantity completion:(GRSNetworkNoResponseNetworkCompletionBlock)completion;
+- (void)incrementInventory:(GRSNetworkNoResponseNetworkCompletionBlock)completion;
+- (void)setInventoryQuantity:(NSInteger)quantity completion:(GRSNetworkNoResponseNetworkCompletionBlock)completion;
+- (void)setNoInventory:(GRSNetworkNoResponseNetworkCompletionBlock)completion;
 
 @end
--- Grocery Store Client/Categories/Product+API_Interaction.m
+++ Grocery Store Client/Categories/Product+API_Interaction.m
@@ -7,7 +7,62 @@
 //
 
 #import "Product+API_Interaction.h"
+#import "VOKCoreDataManager.h"
 
 @implementation Product (API_Interaction)
+
+- (void)purchase:(GRSNetworkNoResponseNetworkCompletionBlock)completion
+{
+    [[GRSNetworkAPIUtility sharedUtility] purchaseProductWithName:self.name completion:^(NSDictionary *userInfo, NSError *error){
+        [self completionHelper:userInfo error:error completion:completion];
+    }];
+}
+
+- (void)purchaseQuantitiy:(NSInteger)quantity completion:(GRSNetworkNoResponseNetworkCompletionBlock)completion
+{
+    [[GRSNetworkAPIUtility sharedUtility] purchaseProductWithName:self.name quantity:quantity completion:^(NSDictionary *userInfo, NSError *error) {
+        [self completionHelper:userInfo error:error completion:completion];
+    }];
+}
+
+- (void)completionHelper:(NSDictionary *)userInfo error:(NSError *)error completion:(GRSNetworkNoResponseNetworkCompletionBlock)completion
+{
+    if (userInfo != nil) {
+        self.quantity = (NSNumber *)[userInfo objectForKey:self.name];
+        [[VOKCoreDataManager sharedInstance] saveMainContext];
+    }
+    
+    if (completion != nil) {
+        completion(error);
+    }
+}
+
+- (void)incrementInventory:(GRSNetworkNoResponseNetworkCompletionBlock)completion
+{
+    [[GRSNetworkAPIUtility sharedUtility] incrementInventoryQuantityForProductWithName:self.name completion:^(NSDictionary *userInfo, NSError *error) {
+        [self completionHelper:userInfo error:error completion:completion];
+    }];
+}
+
+- (void)setInventoryQuantity:(NSInteger)quantity completion:(GRSNetworkNoResponseNetworkCompletionBlock)completion
+{
+    [[GRSNetworkAPIUtility sharedUtility] setInventoryQuantity:quantity toProductWithName:self.name completion:^(NSDictionary *userInfo, NSError *error) {
+        [self completionHelper:userInfo error:error completion:completion];
+    }];
+}
+
+- (void)setNoInventory:(GRSNetworkNoResponseNetworkCompletionBlock)completion
+{
+    [[GRSNetworkAPIUtility sharedUtility] removeAllStockForProductWithName:self.name completion:^(NSError *error){
+        if (error == nil) {
+            self.quantity = @0;
+            [[VOKCoreDataManager sharedInstance] saveMainContext];
+        }
+        
+        if (completion != nil) {
+            completion(error);
+        }
+    }];
+}
 
 @end
